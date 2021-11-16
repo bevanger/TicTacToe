4c90ac994bb206a9031093316c7e50592cad9025
bevanger <baileyevanger@gmail.com>
2021-11-16 13:26:46 -0700

    updating main with code

---
+++ src/Components/Square.js
@@ -0,0 +1,11 @@
+import "../App.css";
+
+function Square({value, chooseSquare}) {
+    return (
+        <div className="square" onClick={chooseSquare}>
+            {value}
+        </div>
+    )
+}
+
+export default Square;
